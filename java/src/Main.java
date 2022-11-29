72144bbf4dedbaca2abf0f38e71f7dcd6f00055c
Nima Hashemi <n.hashemi@ebcom.ir>
2022-11-29 11:47:38 +0330

    fix: main java

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to my challenge world!");
+        System.out.println("Hello world!");
     }
 }
